80c8617d7e67a7ed551037a4b491927a0c72cd15
K4UNG <96415401+K4UNG@users.noreply.github.com>
2022-03-01 08:58:09 +0630

    Update API URL

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const heading = document.getElementById('header');
 const quote = document.getElementById('quote');
 
 async function fetchQuote() {
-    let data = await fetch("https://api.adviceslip.com/advice");
+    let data = await fetch("https://api.adviceslip.com/advice?_" + new Date().getTime());
     let quoteData = await data.json();
     heading.innerText = `ADVICE #${quoteData.slip.id}`;
     quote.innerText = quoteData.slip.advice;
